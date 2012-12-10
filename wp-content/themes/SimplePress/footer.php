4a1bcba60a0711df753f974e96fc8c5fc824bc34
Nicolas <nicolascc.uece@gmail.com>
2012-12-10 17:28:29 -0200

    Coloquei a imagem no fundo da pagina, coloquei a imagem da acens no footer e alterei o css do menu

--- wp-content/themes/SimplePress/footer.php
+++ wp-content/themes/SimplePress/footer.php
@@ -1,6 +1,6 @@
 <br class="clear" />
 <div id="footer">
-	<?php esc_html_e('Direitos reservados','SimplePress'); ?> <a href="http://www.acens.com.br" title="Acens"><img id="logoAcens" src="<?php bloginfo('template_url'); ?> /images/Logos/Acens.png"/></a>
+	<?php esc_html_e('Direitos reservados','SimplePress'); ?> <a href="http://www.acens.com.br" title="Acens"><img id="logoAcens" src="<?php bloginfo('template_url'); ?> /images/Logos/acens.png"/></a>
 </div>
 <?php wp_footer(); ?>	
 <?php get_template_part('includes/scripts'); ?>
